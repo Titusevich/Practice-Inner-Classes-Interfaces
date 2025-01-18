d86d1e6de06e07367ccd21806183fc25bdd79896
Titusevich <nickitatitaevskiy@gmail.com>
2025-01-18 13:46:55 +0200

    Add new method getData in class AppView

--- src/app/AppView.java
+++ src/app/AppView.java
@@ -1,4 +1,15 @@
 package app;
 
+import java.util.Scanner;
+
 public class AppView {
+
+    public String[] getData() {
+        Scanner scan = new Scanner(System.in);
+        System.out.println("Input your message: ");
+        String text = scan.nextLine().trim();
+        System.out.println("Input your name: ");
+        String user = scan.nextLine().trim();
+        return new String[]{text, user};
+    }
 }
