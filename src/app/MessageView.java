f6f56ec68eafdebe366d81f1a53165e5e10db82a
Titusevich <nickitatitaevskiy@gmail.com>
2025-01-18 13:37:19 +0200

    Add new static inner class Message and its constructor in class MessageView

--- src/app/MessageView.java
+++ src/app/MessageView.java
@@ -1,4 +1,30 @@
 package app;
 
 public class MessageView {
+
+    public static class Message {
+        private String text;
+        private String sender;
+
+        public Message(String text, String sender) {
+            this.text = text;
+            this.sender = sender;
+        }
+
+        public String getText() {
+            return text;
+        }
+
+        public void setText(String text) {
+            this.text = text;
+        }
+
+        public String getSender() {
+            return sender;
+        }
+
+        public void setSender(String sender) {
+            this.sender = sender;
+        }
+    }
 }
